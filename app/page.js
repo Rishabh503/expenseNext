a2b99e04a39f80631d6e05f858f0ed8ea62d19c6
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-09-01 01:00:21 +0530

    final chanhes

--- app/page.js
+++ app/page.js
@@ -7,6 +7,8 @@ import RecordHistory from '@/components/RecordHistory';
 import { currentUser } from '@clerk/nextjs/server';
 
 export default async function HomePage() {
+  //just to see
+  
   const user = await currentUser();
   if (!user) {
     return <Guest />;
